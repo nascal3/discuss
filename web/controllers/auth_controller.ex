c5d373ccf5315578448c94b6e4e54af9bd4183ba
caleb.nasio@tuskys.com <caleb.nasio@tuskys.com>
2019-03-15 15:15:31 +0300

    created process to fetch token and information from github and insert it into the users DB

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,12 +1,25 @@
 defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
+  alis Discuss.User
 
   def callback(conn, params) do
-    IO.puts "++++++"
-    IO.inspect(conn.assigns)
-    IO.puts "++++++"
-    IO.inspect(params)
-    IO.puts "++++++"
+    %{assigns: %{ueberauth_auth: auth}} = conn
+#
+    IO.inspect(auth)
+
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
+
+    insert_or_update_user(changeset)
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+       {:ok, user}
+    end
   end
 end
