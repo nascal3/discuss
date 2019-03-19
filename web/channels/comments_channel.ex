fb020be2f03b8b8e87c4ba927f9a4ccd175a78f5
caleb.nasio@tuskys.com <caleb.nasio@tuskys.com>
2019-03-19 17:48:44 +0300

    added function to add comment and user id attached to creator of the comment

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -15,10 +15,11 @@ defmodule Discuss.CommentsChannel do
   def handle_in(name, message, socket) do
     %{"content" => content} = message
     topic = socket.assigns.topic
+    user_id = socket.assigns.user_id
 
     changeset =
       topic
-      |> build_assoc(:comments)
+      |> build_assoc(:comments, user_id: user_id)
       |> Comment.changeset(%{content: content})
 
     case Repo.insert(changeset) do
