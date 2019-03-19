e4addcf8202bf1c3566373310be7bb990c546e20
caleb.nasio@tuskys.com <caleb.nasio@tuskys.com>
2019-03-19 13:57:24 +0300

    corrected typo

--- web/models/comment.ex
+++ web/models/comment.ex
@@ -1,7 +1,7 @@
 defmodule Discuss.Comment do
  use Discuss.Web, :model
 
- @derive {poison.Encoder, only: [:content]}
+ @derive {Poison.Encoder, only: [:content]}
 
  schema "comments" do
    field :content, :string
