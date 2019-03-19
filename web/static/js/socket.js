3289bfb0993c3ae91352ae948216e73dd32783ee
caleb.nasio@tuskys.com <caleb.nasio@tuskys.com>
2019-03-19 11:16:18 +0300

    added a function get comments data from form

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -9,7 +9,13 @@ const createSocket = (topicId) => {
     let channel = socket.channel(`comments:${topicId}`, {});
     channel.join()
         .receive("ok", resp => { console.log("Joined successfully", resp) })
-        .receive("error", resp => { console.log("Unable to join", resp) })
+        .receive("error", resp => { console.log("Unable to join", resp) });
+
+    document.querySelector('button').addEventListener('click', () => {
+        const content = document.querySelector('textarea').value;
+
+        channel.push('comment:add', {content: content});
+    })
 };
 
 window.createSocket = createSocket;
