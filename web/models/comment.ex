4349605954cd51092363a628c6edbb09934b1b35
caleb.nasio@tuskys.com <caleb.nasio@tuskys.com>
2019-03-19 18:37:46 +0300

    added inclusion of user object in poison decoding

--- web/models/comment.ex
+++ web/models/comment.ex
@@ -1,7 +1,7 @@
 defmodule Discuss.Comment do
  use Discuss.Web, :model
 
- @derive {Poison.Encoder, only: [:content, :user_id]}
+ @derive {Poison.Encoder, only: [:content, :user]}
 
  schema "comments" do
    field :content, :string
